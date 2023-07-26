983c10ab7bd06d83d8616829fbe0ec45910d609f
hhttttiger <hhtttttiger@outlook.com>
2023-07-26 22:36:34 +0800

    中间件done

--- framework/group.go
+++ framework/group.go
@@ -2,61 +2,85 @@ package framework
 
 type IGroup interface {
 	// HttpMethod方法
-	Get(string, ControllerHandler)
-	Post(string, ControllerHandler)
-	Put(string, ControllerHandler)
-	Delete(string, ControllerHandler)
+	Get(string, ...ControllerHandler)
+	Post(string, ...ControllerHandler)
+	Put(string, ...ControllerHandler)
+	Delete(string, ...ControllerHandler)
 
 	// 嵌套group
 	Group(string) IGroup
+
+	// 嵌套中间件
+	Use(middlewares ...ControllerHandler)
 }
 
 type Group struct {
 	core   *Core
 	parent *Group
 	prefix string //组名
+
+	middlewares []ControllerHandler // 存放中间件
 }
 
 // 初始化Group
 func NewGroup(core *Core, prefix string) *Group {
 	return &Group{
-		core:   core,
-		parent: nil,
-		prefix: prefix,
+		core:        core,
+		parent:      nil,
+		prefix:      prefix,
+		middlewares: []ControllerHandler{},
 	}
 }
 
+// 注册中间件
+func (g *Group) Use(middlewares ...ControllerHandler) {
+	g.middlewares = append(g.middlewares, middlewares...)
+}
+
 // 实现Get方法
-func (g *Group) Get(uri string, handler ControllerHandler) {
+func (g *Group) Get(uri string, handlers ...ControllerHandler) {
 	uri = g.getAbsolutePrefix() + uri
-	g.core.Get(uri, handler)
+	allHandlers := append(g.getMiddlewares(), handlers...)
+	g.core.Get(uri, allHandlers...)
 }
 
 // 实现Post方法
-func (g *Group) Post(uri string, handler ControllerHandler) {
+func (g *Group) Post(uri string, handlers ...ControllerHandler) {
 	uri = g.getAbsolutePrefix() + uri
-	g.core.Post(uri, handler)
+	allHandlers := append(g.getMiddlewares(), handlers...)
+	g.core.Post(uri, allHandlers...)
 }
 
 // 实现Put方法
-func (g *Group) Put(uri string, handler ControllerHandler) {
+func (g *Group) Put(uri string, handlers ...ControllerHandler) {
 	uri = g.getAbsolutePrefix() + uri
-	g.core.Put(uri, handler)
+	allHandlers := append(g.getMiddlewares(), handlers...)
+	g.core.Put(uri, allHandlers...)
 }
 
 // 实现Delete方法
-func (g *Group) Delete(uri string, handler ControllerHandler) {
+func (g *Group) Delete(uri string, handlers ...ControllerHandler) {
 	uri = g.getAbsolutePrefix() + uri
-	g.core.Delete(uri, handler)
+	allHandlers := append(g.getMiddlewares(), handlers...)
+	g.core.Delete(uri, allHandlers...)
 }
 
-// 递归获取组名
+// 获取当前group的绝对路径
 func (g *Group) getAbsolutePrefix() string {
 	if g.parent == nil {
 		return g.prefix
 	}
+	return g.parent.getAbsolutePrefix() + g.prefix
+}
 
-	return g.getAbsolutePrefix() + g.prefix
+// 获取某个group的middleware
+// 这里就是获取除了Get/Post/Put/Delete之外设置的middleware
+func (g *Group) getMiddlewares() []ControllerHandler {
+	if g.parent == nil {
+		return g.middlewares
+	}
+
+	return append(g.parent.getMiddlewares(), g.middlewares...)
 }
 
 // 实现 Group 方法
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"ASPGo/framework"
+	"ASPGo/middleware"
 	"ASPGo/route"
 	"net/http"
 )
 
 func main() {
 	core := framework.NewCore()
+	core.Use(middleware.Recovery())
+
 	route.RegisterRouter(core)
 	server := &http.Server{
 		Handler: core,

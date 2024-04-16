f1daaa352823a03d0eda6d2a3b2fb8882baa945a
tiger <hhtttttiger@outlook.com>
2024-04-16 20:58:59 +0800

    去除cobra，加上这玩意实在是太重了

--- go.mod
+++ go.mod
@@ -41,11 +41,17 @@ require (
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/sqlite v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
 )
 
 require (
@@ -59,4 +65,5 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/swaggo/swag v1.7.3
 	gopkg.in/fsnotify.v1 v1.4.7
+	gorm.io/driver/mysql v1.5.2
 )
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	console "ASPGo/app/console/command"
-	"ASPGo/app/http"
+	asphttp "ASPGo/app/http"
 	"ASPGo/framework"
+	"ASPGo/framework/contract"
 	"ASPGo/framework/provider/app"
 	"ASPGo/framework/provider/config"
 	"ASPGo/framework/provider/env"
 	"ASPGo/framework/provider/kernel"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -21,10 +28,38 @@ func main() {
 	container.Bind(&config.HadeConfigProvider{})
 
 	// 将HTTP引擎初始化,并且作为服务提供者绑定到服务容器中
-	if engine, err := http.NewHttpEngine(); err == nil {
+	if engine, err := asphttp.NewHttpEngine(); err == nil {
 		container.Bind(&kernel.HadeKernelProvider{HttpEngine: engine})
 	}
 
-	// 运行root命令
-	console.RunCommand(container)
+	// 从服务容器中获取kernel的服务实例
+	kernelService := container.MustMake(contract.KernelKey).(contract.Kernel)
+	// 从kernel服务实例中获取引擎
+	core := kernelService.HttpEngine()
+
+	// 创建一个Server服务
+	server := &http.Server{
+		Handler: core,
+		Addr:    ":8888",
+	}
+
+	// 这个goroutine是启动服务的goroutine
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	// 当前的goroutine等待信号量
+	quit := make(chan os.Signal)
+	// 监控信号：SIGINT, SIGTERM, SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 这里会阻塞当前goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(timeoutCtx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
 }
